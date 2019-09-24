5a72b21fd46d11223a862a548a9de2fb93f5ab45
Jeremy Smith <jeremysmith@Jeremys-iMac.fios-router.home>
2019-09-23 23:22:27 -0400

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,13 @@ import App from 'next/app';
 import React from 'react';
 import Layout from '../components/layout';
 
-
 export default class MyApp extends App {
-
   render() {
     const { Component, pageProps, router } = this.props;
     return (
-          <Layout>
-            <Component {...pageProps} key={router.pathname} />
-          </Layout>
+      <Lafyout>
+        <Component {...pageProps} key={router.pathname} />
+      </Lafyout>
     );
   }
 }
