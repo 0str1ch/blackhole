52519a0ad2ca8f58fa8ff439da02747cb2759d7e
Jeremy Smith <jeremysmith@Jeremys-iMac.fios-router.home>
2019-09-24 00:12:08 -0400

    reorder

--- components/canvas-background.js
+++ components/canvas-background.js
@@ -101,6 +101,31 @@ const Sphere = () => {
   );
 };
 
+function Stars() {
+  const group = useRef();
+  const [geo, mat, vertices, coords] = useMemo(() => {
+    const geo = new THREE.SphereBufferGeometry(0.5, 10, 10);
+    const mat = new THREE.MeshBasicMaterial({
+      color: new THREE.Color('lightblue')
+    });
+    const coords = new Array(2000)
+      .fill()
+      .map(i => [
+        Math.random() * 800 - 400,
+        Math.random() * 3800 - 400,
+        Math.random() * 800 - 400
+      ]);
+    return [geo, mat, vertices, coords];
+  }, []);
+  return (
+    <group ref={group}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      ))}
+    </group>
+  );
+}
+
 function Effect() {
   const composer = useRef();
   const { scene, gl, size, camera } = useThree();
@@ -127,31 +152,6 @@ function Effect() {
         renderToScreen
       />
     </effectComposer>
-  );
-}
-
-function Stars() {
-  const group = useRef();
-  const [geo, mat, vertices, coords] = useMemo(() => {
-    const geo = new THREE.SphereBufferGeometry(0.5, 10, 10);
-    const mat = new THREE.MeshBasicMaterial({
-      color: new THREE.Color('lightblue')
-    });
-    const coords = new Array(2000)
-      .fill()
-      .map(i => [
-        Math.random() * 800 - 400,
-        Math.random() * 3800 - 400,
-        Math.random() * 800 - 400
-      ]);
-    return [geo, mat, vertices, coords];
-  }, []);
-  return (
-    <group ref={group}>
-      {coords.map(([p1, p2, p3], i) => (
-        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
-      ))}
-    </group>
   );
 }
 
